38eabcf2ae51ddc74d8f7b5df702879f630c6410
W0lfH0und414 <alex.svetlov47@gmail.com>
2020-05-04 16:02:12 +0300

    Updates

---
+++ precache-manifest.f2d1e045255de60c217e491a006de849.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1633b4844fc8b71cad9f0841f3cda775",
+    "url": "/react-trisemus-app/index.html"
+  },
+  {
+    "revision": "ec103c2a251d2aeeeb50",
+    "url": "/react-trisemus-app/static/css/main.1b93776e.chunk.css"
+  },
+  {
+    "revision": "c761015b87db2354a3cd",
+    "url": "/react-trisemus-app/static/js/2.d3b3d68a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-trisemus-app/static/js/2.d3b3d68a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec103c2a251d2aeeeb50",
+    "url": "/react-trisemus-app/static/js/main.f78e62a5.chunk.js"
+  },
+  {
+    "revision": "3edac2760e43c8a69d72",
+    "url": "/react-trisemus-app/static/js/runtime-main.b42d23b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-trisemus-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
